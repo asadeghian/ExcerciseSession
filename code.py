ab2554e11dd4ff1fbf1ff3c8dbbb268dc227cba4
amirabbas sadeghian <amir.abbas00@gmail.com>
2012-02-21 18:14:11 +0100

    find_bad_gender part2 is finished

--- code.py
+++ code.py
@@ -1,9 +1,18 @@
 import os,glob
 
+def find_bad_gender(file_line):
+	if file_line.find(': N') == -1:
+		return 0
+	return 1
+
 def find_file_names(path):
 	return glob.glob(path+'/*/*.txt')
 
 def main():
-	print find_file_names('./cleaneddata')
+	for i in find_file_names('./cleaneddata'):
+		f=open(i)
+		for line in f:
+			if find_bad_gender(line) == 1:
+
 
 main()
